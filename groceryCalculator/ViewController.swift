f06fb4bd3745539ddedb8fe6939307add7771545
Mike Camp <mike_camp23@icloud.com>
2021-03-30 12:16:52 -0400

    Initial Commit

---
+++ groceryCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  groceryCalculator
+//
+//  Created by Michael Camp on 3/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
